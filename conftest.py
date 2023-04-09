c7177a4c00cd1a16fcda5307eddac4b11c051e01
Dmitrii <dimasik007@rambler.ru>
2023-04-09 23:18:21 +0300

    Добавил тест для логина

---
+++ conftest.py
@@ -0,0 +1,41 @@
+import json
+import pytest
+from fixture.application import Application
+import os.path
+
+fixture = None
+conf = None
+
+
+def load_config(file):
+    global conf
+    if conf is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                   file)
+        with open(config_file) as config_file:
+            conf = json.load(config_file)
+    return conf
+
+
+@pytest.fixture
+def app(request):
+    global fixture
+    browser = request.config.getoption("--browser")
+    web_config = load_config(request.config.getoption("--conf"))['web']
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, base_url=web_config["baseUrl"])
+    return fixture
+
+
+@pytest.fixture(scope="session", autouse=True)
+def stop(request):
+    def fin():
+        fixture.session.ensure_logout()
+        fixture.destroy()
+    request.addfinalizer(fin)
+    return fixture
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="chrome")
+    parser.addoption("--conf", action="store", default="conf.json")
