2af41000eb2d969816d84a82074a6fa370871a52
Dmitrii <dimasik007@rambler.ru>
2023-04-13 22:44:34 +0300

    Добавил тест на создание пользователя

--- fixture/application.py
+++ fixture/application.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
-
 from fixture.session import SessionHelper
 from fixture.project import ProjectHelper
+from fixture.james import JamesHelper
 
 
 class Application:
@@ -18,6 +18,7 @@ class Application:
         #       self.wd.implicitly_wait(5)
         self.session = SessionHelper(self)
         self.project = ProjectHelper(self)
+        self.james = JamesHelper(self)
         self.base_url = base_url
 
     def is_valid(self):
