621ee55807f24927b13677ff0502b2003db135cc
Dmitrii <dimasik007@rambler.ru>
2023-04-13 22:56:38 +0300

    Вынес данные для входа в james в config

--- fixture/application.py
+++ fixture/application.py
@@ -6,7 +6,7 @@ from fixture.james import JamesHelper
 
 class Application:
 
-    def __init__(self, browser, base_url):
+    def __init__(self, browser, config):
         if browser == "chrome":
             self.wd = webdriver.Chrome()
         elif browser == "firefox":
@@ -19,7 +19,8 @@ class Application:
         self.session = SessionHelper(self)
         self.project = ProjectHelper(self)
         self.james = JamesHelper(self)
-        self.base_url = base_url
+        self.config = config
+        self.base_url = config['web']['baseUrl']
 
     def is_valid(self):
         try:
