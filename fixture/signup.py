f238817b863af722d5e760dea3760e683a8181c9
Dmitrii <dimasik007@rambler.ru>
2023-04-14 09:08:36 +0300

    Дорабатывал тест регистрации нового пользователя

--- fixture/signup.py
+++ fixture/signup.py
@@ -1,3 +1,5 @@
+import re
+
 class SignUpHelper:
 
     def __init__(self, app):
@@ -5,18 +7,20 @@ class SignUpHelper:
 
     def new_user(self, username, password, email):
         wd = self.app.wd
-        wd.get(self.app.baseUrl + "/signup_page.php")
+        wd.get(self.app.base_url + "/signup_page.php")
         wd.find_element_by_name("username").send_keys(username)
         wd.find_element_by_name("email").send_keys(email)
         wd.find_element_by_css_selector("input[type='submit']").click()
 
-        mail = self.app.mail.get_mail("[MantisBT] Account registration")
+        mail = self.app.mail.get_mail(username, password, "[MantisBT] "
+                                                          "Account "
+                                               "registration")
         url = self.extract_confirmation_url(mail)
 
         wd.get(url)
         wd.find_element_by_name("password").send_keys(password)
-        wd.find_element_by_name("password1").send_keys(password)
-        wd.find_element_by_css_selector("input[type='submit']").click()
+        wd.find_element_by_name("password_confirm").send_keys(password)
+        wd.find_element_by_css_selector("input[value='Update User']").click()
 
-    def extract_confirmation_url(self):
-        pass
+    def extract_confirmation_url(self, text):
+        return re.search("http://.*$", text, re.MULTILINE).group(0)
